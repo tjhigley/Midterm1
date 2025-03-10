3bc67177b2c07ff2f55b9ad7e23cef644e428a21
tjhigley <tjhigley5@gmail.com>
2025-03-10 13:06:54 -0600

    Add project files.

---
+++ Midterm1.cpp
@@ -0,0 +1,26 @@
+﻿#include "Midterm1.h"
+
+using namespace std;
+
+enum Tier {
+	LOW,
+	MEDIUM,
+	HIGH
+};
+
+class Location {
+public:
+
+};
+
+class Person {
+public:
+	enum Tier Tier;
+
+};
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
